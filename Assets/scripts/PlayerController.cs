71a99f84b53fe64a5addcba4d3945063e5160f90
greysome <wayyan.win@gmail.com>
2018-02-12 23:49:45 +0800

    make player movement smoother

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -4,23 +4,39 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
     private Rigidbody2D rb;
-    private float rotate_speed = 3f;
-    private float forward_speed = 7f;
+    private float rotate_speed = 4f;
+    private float max_forward_speed = 7f;
+    private float min_forward_speed = 5f;
     private float deceleration = 0.99f;
+    private bool rotating = true;
     
     void Start() {
 	rb = GetComponent<Rigidbody2D>();
     }
 
     void FixedUpdate() {
-	if (Input.GetButton("player_left"))
+	if (Input.GetButton("player_left")) {
+	    rotating = true;
 	    transform.Rotate(Vector3.forward * rotate_speed);
+	}
 
-	if (Input.GetButton("player_right"))
+	if (Input.GetButton("player_right")) {
+	    rotating = true;
 	    transform.Rotate(Vector3.back * rotate_speed);
+	}
 
-	if (Input.GetButton("player_forward"))
-	    rb.velocity = transform.up * forward_speed;
+	if (!Input.GetButton("player_left") && !Input.GetButton("player_right")) {
+	    rotating = false;
+	}
+
+	if (Input.GetButton("player_forward")) {
+	    if (rotating) {
+		rb.velocity = transform.up * min_forward_speed;
+	    }
+	    else {
+		rb.velocity = transform.up * max_forward_speed;
+	    }
+	}
 	else
 	    rb.velocity = deceleration * rb.velocity;
     }
