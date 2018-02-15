fc387eab1383695a037a085345d57c08bfd03347
greysome <wayyan.win@gmail.com>
2018-02-16 00:36:53 +0800

    destroy bullets upon exceeding boundary

--- Assets/scripts/BulletController.cs
+++ Assets/scripts/BulletController.cs
@@ -6,11 +6,20 @@ public class BulletController : MonoBehaviour {
     private Rigidbody2D rb;
     private GameObject player;
 
+    public GameObject self;
     public float speed = 100f;
+    public float destroy_threshold = 100f;
 
     void Start() {
 	player = GameObject.FindWithTag("player_1");
 	rb = GetComponent<Rigidbody2D>();
 	rb.velocity = transform.up * speed;
+    }
+
+    void Update() {
+	if (Mathf.Abs(transform.position.x) > destroy_threshold ||
+	    Mathf.Abs(transform.position.y) > destroy_threshold) {
+	    Object.Destroy(this.self);
+	}
     }
 }
